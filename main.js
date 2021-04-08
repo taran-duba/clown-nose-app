69ac78928e27b9be85d340fbd9420136b55febe2
taran-duba <72531052+taran-duba@users.noreply.github.com>
2021-04-07 20:52:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var noseX = 0;
+var noseY = 0;
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/brqXCKX3/clown-nose.png');
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log("Welcome to PoseNet!");
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 50, 50);
+}
+function take_snapshot() {
+    save('myFilterImage.jpg');
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
